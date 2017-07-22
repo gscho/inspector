856aa3ecd0272f2480341ea8527da5aa12085d8e
Greg Schofield <gregs@indellient.com>
2017-07-22 17:05:41 -0400

    Added navbar, table bootstrap classes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module InspectorApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.precompile += %w( application.css application.js )
+    config.assets.precompile += %w( application.css.scss application.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'test_plans#index'
   get 'test_plans/:id/execute', to: 'test_plans#execute'
 
   resources :test_plans
